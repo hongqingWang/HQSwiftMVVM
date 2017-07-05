2476006e1337df9172ac4ef4bb8bf831dc1bad2b
hongqingWang <272338444@qq.com>
2017-07-05 22:06:49 +0800

    设置项目文件结构

--- HQSwiftMVVM/HQSwiftMVVM/AppDelegate.swift
+++ HQSwiftMVVM/HQSwiftMVVM/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        sleep(2)
+        window = UIWindow()
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = HQMainViewController()
+        window?.makeKeyAndVisible()
         
         return true
     }
---
+++ HQSwiftMVVM/HQSwiftMVVM/Classes/View/D/HQDViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HQDViewController.swift
+//  HQSwiftMVVM
+//
+//  Created by 王红庆 on 2017/7/5.
+//  Copyright © 2017年 王红庆. All rights reserved.
+//
+
+import UIKit
+
+class HQDViewController: HQBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
