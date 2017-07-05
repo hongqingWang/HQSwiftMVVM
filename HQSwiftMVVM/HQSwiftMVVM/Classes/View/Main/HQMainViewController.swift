09b14542be37df7c8c783b90aceb8bdf852c7a25
hongqingWang <272338444@qq.com>
2017-07-05 22:31:26 +0800

    设置MainViewController内的子控制器，未完

--- HQSwiftMVVM/HQSwiftMVVM/Classes/View/Main/HQMainViewController.swift
+++ HQSwiftMVVM/HQSwiftMVVM/Classes/View/Main/HQMainViewController.swift
@@ -13,23 +13,37 @@ class HQMainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupChildControllers()
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+/*
+ extension 类似于 OC 中的分类,在 Swift 中还可以用来切分代码块
+ 可以把功能相近的函数,放在一个extension中
+ */
+extension HQMainViewController {
+    
+    
+    /// 设置所有子控制器
+    func setupChildControllers() {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    /// 使用字典创建一个子控制器
+    ///
+    /// - Parameter dict: 信息字典[className, title, imageName]
+    /// - Returns: 子控制器
+//    func controller(dict: [String: String]) -> UIViewController {
+//        
+//        // 1. 获取字典内容
+//        guard let className = dict["className"],
+//            let title = dict["title"],
+//            let imageName = dict["imageName"] else {
+//                
+//                return UIViewController()
+//        }
+//        // 2. 创建视图控制器
+//        
+//    }
 }
