16b7550ac4b1865b1e579b4b16f1e01fc19ed154
hongqingWang <272338444@qq.com>
2017-07-05 21:22:40 +0800

    初始化项目
    初始化项目

---
+++ HQSwiftMVVM/HQSwiftMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HQSwiftMVVM
+//
+//  Created by 王红庆 on 2017/7/5.
+//  Copyright © 2017年 王红庆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
