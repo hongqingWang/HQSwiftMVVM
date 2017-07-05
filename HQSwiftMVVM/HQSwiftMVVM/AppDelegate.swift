238b46075cc97e88944e7e7498c39cffda18f8af
hongqingWang <272338444@qq.com>
2017-07-05 21:47:28 +0800

    添加`APPIcon`和`LaunchImage`图片

--- HQSwiftMVVM/HQSwiftMVVM/AppDelegate.swift
+++ HQSwiftMVVM/HQSwiftMVVM/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        sleep(2)
+        
         return true
     }
 
