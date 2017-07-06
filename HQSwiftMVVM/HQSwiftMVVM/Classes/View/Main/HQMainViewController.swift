b4a35565eca0192c9ea4f836426042a4878c6f67
hongqingWang <272338444@qq.com>
2017-07-06 15:26:31 +0800

    设置子控制器的`TabBar`图片和标题颜色及大小

--- HQSwiftMVVM/HQSwiftMVVM/Classes/View/Main/HQMainViewController.swift
+++ HQSwiftMVVM/HQSwiftMVVM/Classes/View/Main/HQMainViewController.swift
@@ -28,7 +28,10 @@ extension HQMainViewController {
     fileprivate func setupChildControllers() {
         
         let array = [
-            ["className": "HQAViewController", "title": "首页", "imageName": ""]
+            ["className": "HQAViewController", "title": "首页", "imageName": "a"],
+            ["className": "HQBViewController", "title": "消息", "imageName": "b"],
+            ["className": "HQCViewController", "title": "发现", "imageName": "c"],
+            ["className": "HQDViewController", "title": "我", "imageName": "d"]
         ]
         var arrayM = [UIViewController]()
         for dict in array {
@@ -58,9 +61,23 @@ extension HQMainViewController {
                 
                 return UIViewController()
         }
+        
         // 2. 创建视图控制器
         let vc = cls.init()
         vc.title = title
+        
+        // 3. 设置图像
+        vc.tabBarItem.image = UIImage(named: "tabbar_" + imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: "tabbar_" + imageName + "_selected")?.withRenderingMode(.alwaysOriginal)
+        // 设置`tabBar`标题颜色
+        vc.tabBarItem.setTitleTextAttributes(
+            [NSForegroundColorAttributeName: UIColor.orange],
+            for: .selected)
+        // 设置`tabBar`标题字体大小,系统默认是`12`号字
+        vc.tabBarItem.setTitleTextAttributes(
+            [NSFontAttributeName: UIFont.systemFont(ofSize: 12)],
+            for: .normal)
+        
         let nav = HQNavigationController(rootViewController: vc)
         return nav
     }
