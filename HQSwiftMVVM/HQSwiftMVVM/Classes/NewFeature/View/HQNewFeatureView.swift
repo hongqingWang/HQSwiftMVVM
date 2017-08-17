6c8b17f6e73d65803c37ddf5dfa9b72552e2697c
hongqingWang <272338444@qq.com>
2017-08-17 17:21:35 +0800

    准备欢迎视图和新特性视图

---
+++ HQSwiftMVVM/HQSwiftMVVM/Classes/NewFeature/View/HQNewFeatureView.swift
@@ -0,0 +1,22 @@
+//
+//  HQNewFeatureView.swift
+//  HQSwiftMVVM
+//
+//  Created by 王红庆 on 2017/8/17.
+//  Copyright © 2017年 王红庆. All rights reserved.
+//
+
+import UIKit
+
+class HQNewFeatureView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.orange
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ HQSwiftMVVM/HQSwiftMVVM/Classes/NewFeature/View/HQWelcomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HQWelcomeView.swift
+//  HQSwiftMVVM
+//
+//  Created by 王红庆 on 2017/8/17.
+//  Copyright © 2017年 王红庆. All rights reserved.
+//
+
+import UIKit
+
+class HQWelcomeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
